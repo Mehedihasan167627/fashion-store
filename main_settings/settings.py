7ea10cc0b7f36c3bb5df13cbd01fd12c68048b43
mehedihasan <mehedinahid27@gmail.com>
2022-10-31 14:09:09 +0600

    add media url

--- main_settings/settings.py
+++ main_settings/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = '/download/images/'
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
